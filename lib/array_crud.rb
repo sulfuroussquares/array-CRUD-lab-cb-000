c8c51da3989d9e85036fdf7ba2da45dbe9b951d0
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-30 18:41:51 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,43 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+array1 = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+array.push(element)
 end
+#This method takes in two arguments, an array and the element we want to add to it.
+#Use the `<<` (shovel) method or the `#push` method to add that element to the end of the new array.
 
 def add_element_to_start_of_array(array, element)
-  
+  array.unshift(element)
+
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array.pop
+
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
+
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array[index_number]
+
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.first
+
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
+
 end
