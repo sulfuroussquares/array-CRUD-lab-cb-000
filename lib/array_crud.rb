44dfd1a2b2b17e24dfe2f1bdce03b7b38cfb082c
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-30 18:39:09 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,10 @@ array1 = [1, 2, 3, 4]
 end
 
 def add_element_to_end_of_array(array, element)
-
+array = []
 end
+#This method takes in two arguments, an array and the element we want to add to it. 
+#Use the `<<` (shovel) method or the `#push` method to add that element to the end of the new array.
 
 def add_element_to_start_of_array(array, element)
 
